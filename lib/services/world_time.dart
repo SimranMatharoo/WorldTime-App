a7d64dd551eed619d8ce15d1a69f8845b8d854fc
SimranMatharoo <simmatharoo2741@gmail.com>
2021-05-12 23:55:35 +0530

    modifications

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -1,5 +1,9 @@
 import 'package:http/http.dart';
 import 'dart:convert';
+import 'package:intl/intl.dart';
+
+//intl package is used to format various kinds of data. In this project, we are formatting time.
+
 
 class WorldTime {
   // Use the late keyword to initialize a variable when it is first read,
@@ -24,17 +28,22 @@ class WorldTime {
 
       //getting properties from data
       String datetime = data['datetime'];
-      String offset = data['utc_offset'].substring(1,3);
+      String offset1 = data['utc_offset'].substring(1,3);
+      String offset2 = data['utc_offset'].substring(4,6);
+
+      DateTime now = DateTime.parse(datetime);
+
+      print(now);
       //print(datetime);
-      print(offset);
+
 
       //create DateTime object
-      DateTime now = DateTime.parse(datetime);
-      now = now.add(Duration(hours: int.parse(offset)));
+      now = now.add(Duration(hours: int.parse(offset1), minutes: int.parse(offset2))); // if offset is in minutes too.
+     // now = now.add(Duration(hours: int.parse(offset)));
       //print(now);
 
 
-      time = now.toString();
+      time = DateFormat.jm().format(now);
 
 
     } catch (e) {
